c476cf98c0fc65e84a3e9f8beea05d252bdb006f
Michael Herman <hermanmu@gmail.com>
2015-10-31 21:45:48 -0600

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# generator-galvanize-mean
+
+[Yeoman](http://yeoman.io) generator for a basic MEAN (MongoDB, ExpressJS, AngularJS, and NodeJS) boilerplate.
+
+## Quick Start
+
+1. Install Yeoman (if necessary) - `npm install -g yo`
+1. Install the generator - `npm install -g generator-galvanize-mean`
+1. Run - `yo galvanize-mean`
+1. Install modules - `npm install`
+1. Fire up the server - `gulp` (or create a build - `gulp build`)
---
+++ app/index.js
@@ -0,0 +1,23 @@
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+var GalvanizeExpreessGenerator = yeoman.generators.Base.extend({
+
+  promptUser: function() {
+    // greeting
+    console.log(chalk.magenta("Welcome to Galvanize's MEAN app Generator"));
+  },
+
+  createApp: function(){
+    this.copy('./src/client/**/*', './src/client/**/*');
+    this.copy('./src/server/**/*', './src/server/**/*');
+    this.copy('package.json');
+    this.copy('_gitignore', '.gitignore');
+    this.copy('gulpfile.js', 'gulpfile.js');
+  },
+
+});
+
+module.exports = GalvanizeExpreessGenerator;
